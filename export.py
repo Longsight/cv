be2f934461664b9989d75e83851a98d22581bf80
Ashleigh Crosby <tcrosby@gmail.com>
2026-01-06 18:05:08 +0000

    begin export again

--- export.py
+++ export.py
@@ -3,33 +3,38 @@ import sqlite3
 import yaml
 from models import *
 
-con = sqlite3.connect('cv-old.db')
+con = sqlite3.connect('cv.db')
 con.row_factory = sqlite3.Row
 cur = con.cursor()
 
 res = cur.execute("""
-                  select categories.category, skills.skill from categories 
-                  join skill_categories on categories.rowid = skill_categories.category
-                  join skills on skill_categories.skill = skills.rowid
-                  order by categories.category asc, skills.skill asc
+                  select skills.name, skills.competency from skills
+                  order by skills.name asc
                   """).fetchall()
 
-categories = {r: Category(r) for r in sorted(set([row["category"] for row in res]))}
-skills = {r: Skill(r, 'Good') for r in sorted(set([row["skill"] for row in res]))}
-
-for row in res:
-    categories[row["category"]].skills.append(skills[row["skill"]])
+skills = {r["name"]: Skill(**r) for r in res}
 
 res = cur.execute("""
-                  select a.rowid as a_id, a.detail, skills.skill as s, roles.rowid as role_id, 
+                  select categories.name, skills.name as skill from categories 
+                  join skill_categories on categories.category_id = skill_categories.category_id
+                  join skills on skill_categories.skill_id = skills.skill_id
+                  order by categories.name asc, skill asc
+                  """).fetchall()
+
+categories = {r["name"]: Category(**r) for r in res}
+
+for row in res:
+    categories[row["name"]].skills.append(skills[row["skill"]])
+
+res = cur.execute("""
+                  select achievements.achievement_id, achievements.detail, skills.name as skill, roles.role_id, 
                   roles.employer, roles.title, roles.location,
-                  strftime('%Y-%m-%d', roles.start) as start_date, strftime('%Y-%m-%d', roles.end) as end_date
-                  from achievements as a
-                  join achievement_skills on a_id = achievement_skills.achievement
-                  join skills on achievement_skills.skill = skills.rowid
-                  left outer join achievement_roles on a_id = achievement_roles.achievement
-                  left outer join roles on achievement_roles.`role` = role_id
-                  order by roles.start desc, a_id asc
+                  strftime('%Y-%m-%d', roles.start_date) as start_date, strftime('%Y-%m-%d', roles.end_date) as end_date
+                  from achievements
+                  join skill_achievements on achievements.achievement_id = skill_achievements.achievement_id
+                  join skills on skill_achievements.skill_id = skills.skill_id
+                  left outer join roles on achievements.role_id = roles.role_id
+                  order by roles.start_date desc, achievements.achievement_id asc
                   """).fetchall()
 
 roles = {r.employer: r for r in sorted(set([Role(**row) for row in res]))}
@@ -46,7 +51,6 @@ doc = {
     "achievements": list(achievements.values()),
 }
 
-stream = open('cv-old.yaml', 'w')
-yaml.dump(doc, stream, indent=2, width=500, sort_keys=False)
+print(yaml.dump(doc, indent=2, width=500, sort_keys=False))
 
 con.close()
