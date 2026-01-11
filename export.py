5e5596d18b3505363e025036391559d1f2bb392f
Ashleigh Crosby <tcrosby@gmail.com>
2026-01-11 16:16:02 +0000

    update grouping

--- export.py
+++ export.py
@@ -8,37 +8,32 @@ con = sqlite3.connect('cv.db')
 con.row_factory = sqlite3.Row
 cur = con.cursor()
 
-res = cur.execute("""
-                  select skills.name, skills.competency from skills
-                  order by skills.name asc
-                  """).fetchall()
-
-skills = {r.name: r for r in set([Skill(**row) for row in res])}
-
-
 def group_rows(type, rows, key_name, sort=False):
     result = []
     for key, group in groupby(rows, lambda x: x[key_name]):
         rows = list(group)
         obj = type(**rows[0])
-        obj.skills = [skills[s] for s in [r["skill"] for r in rows]]
+        obj.skills = {r["skill"]: r["competency"] for r in rows}
         result.append(obj)
     if sort:
         return sorted(result)
     return result
 
-
 res = cur.execute("""
-                  select categories.name, skills.name as skill from categories 
-                  join skill_categories on categories.category_id = skill_categories.category_id
-                  join skills on skill_categories.skill_id = skills.skill_id
-                  order by categories.name asc, skill asc
+                  select skills.name, skills.competency from skills
+                  order by skills.name asc
                   """).fetchall()
 
-categories = group_rows(Category, res, "name")
-# categories = {r.name: r for r in set([Category(**row) for row in res])}
-# for row in res:
-#     categories[row["name"]].skills.append(skills[row["skill"]])
+skills = list(set([Skill(**row) for row in res]))
+
+# res = cur.execute("""
+#                   select categories.name, skills.name as skill, skills.competency from categories 
+#                   join skill_categories on categories.category_id = skill_categories.category_id
+#                   join skills on skill_categories.skill_id = skills.skill_id
+#                   order by categories.name asc, skill asc
+#                   """).fetchall()
+
+# categories = group_rows(Category, res, "name")
 
 res = cur.execute("""
                   select roles.role_id, roles.employer, roles.title, roles.location,
@@ -51,7 +46,7 @@ roles = list(sorted(set([Role(**row) for row in res])))
 
 res = cur.execute("""
                   select achievements.achievement_id, achievements.detail, skills.name as skill,
-                  roles.employer as role from achievements join skill_achievements
+                  skills.competency, roles.employer as role from achievements join skill_achievements
                   on achievements.achievement_id = skill_achievements.achievement_id
                   join skills on skill_achievements.skill_id = skills.skill_id
                   left outer join roles on achievements.role_id = roles.role_id
@@ -59,15 +54,9 @@ res = cur.execute("""
                   """).fetchall()
 
 achievements = group_rows(Achievement, res, "detail")
-# achievements = {r.detail: r for r in set([Achievement(**row) for row in res])}
-
-# for row in res:
-#     achievements[row['detail']].skills.append(skills[row["skill"]])
 
 doc = {
-    "competencies": [Competency(c) for c in Competency],
-    "skills": list(skills.values()),
-    "categories": categories,
+    "skills": skills,
     "roles": roles,
     "achievements": achievements,
 }
